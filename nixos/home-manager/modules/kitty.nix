ce002dbdf456ca5d9ec35073d936afcd4c9bb752
Flexksx <cristian.cretu2808@gmail.com>
2025-02-17 10:08:08 +0200

    update

--- nixos/home-manager/modules/kitty.nix
+++ nixos/home-manager/modules/kitty.nix
@@ -8,6 +8,9 @@
   programs.kitty = {
     enable = true;
     shellIntegration.enableZshIntegration = true;
+    font = {
+      size = lib.mkForce 12;
+    };
     settings = {
       enable_audio_bell = false;
       background_blur = 5;
--- nixos/home-manager/modules/stylix.nix
+++ nixos/home-manager/modules/stylix.nix
@@ -1,8 +1,27 @@
-{...}: {
+{ pkgs, ... }:
+{
   stylix = {
     enable = true;
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-light-soft.yaml";
-    image = ../../../backrgrounds/light/JapaneseShop.jpg;
-    polarity = "light";
+    image = ../../../backrgrounds/dark/Magnolia.jpg;
+    polarity = "dark";
+    fonts = {
+      serif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Serif";
+      };
+      sansSerif = {
+        package = pkgs.noto-fonts;
+        name = "Noto Sans";
+      };
+      monospace = {
+        package = pkgs.ubuntu-sans;
+        name = "Ubuntu Mono";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-color-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
   };
 }
